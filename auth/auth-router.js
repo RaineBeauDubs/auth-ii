a798e2cd5640352709f79310022ec1732e19f919
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-07 16:19:14 -0500

    Can now log in and get token.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,4 +23,45 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res
+          .status(200)
+          .json({
+            message: `Welcome ${user.username}!, here is a token!`,
+            token,
+          });
+      } else {
+        res
+          .status(401)
+          .json({
+            message: "Uhm... I don't think so...."
+          });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json(error);
+    });
+});
+
+function generateToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
 module.exports = router;
